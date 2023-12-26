163472d8ff5545ac44b8624c3dcca4cfff41dab4
Preedee Ponchevin <nickmsft@mac.com>
2023-12-26 15:14:08 +0700

    Initial Project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use log::debug;
+use serde::Deserialize;
+
+#[derive(Debug,Deserialize)]
+struct ExcelRow {
+    service_name: String,
+    average_response_time_95_ms: f64,
+    max_response_time_95_ms: f64,
+    min_response_time_95_ms: f64,
+    count: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    pretty_env_logger::init();
+
+    debug!("Hello, world!");
 }
