3990e33c343c5ed563490f2492c0674d0ced91a2
Preedee Ponchevin <nickmsft@mac.com>
2023-12-26 18:40:19 +0700

    Initial Project

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::io;
+
 use calamine::{open_workbook, Reader, Xlsx};
 use clap::Parser;
 use log::{debug, error, info};
+use rusqlite::{Connection, named_params};
 use serde::Deserialize;
-use sqlite::Connection;
 
 #[derive(Debug, Deserialize)]
 struct ExcelRow {
@@ -24,27 +26,34 @@ struct Cli {
     sheet_name: String,
 }
 
-fn load_excel(file_name: &String, sheet_name: &String) -> Result<Vec<ExcelRow>, calamine::XlsxError> {
-    debug!("Loading excel with file name: {} and sheet name: {}",file_name,sheet_name);
+fn load_excel(
+    file_name: &String,
+    sheet_name: &String,
+) -> Result<Vec<ExcelRow>, calamine::XlsxError> {
+    debug!(
+        "Loading excel with file name: {} and sheet name: {}",
+        file_name, sheet_name
+    );
 
     use std::time::Instant;
     let now = Instant::now();
 
     let wb: Result<Xlsx<_>, calamine::XlsxError> = open_workbook(file_name);
     let wb = wb.map(|mut wb| {
-        let res = wb.worksheet_range(sheet_name)
-            .map(|range| {
-                //skip the first row , for header
-                range.rows().skip(1).map(|row| {
-                    ExcelRow {
-                        service_name: row.get(0).unwrap().get_string().unwrap().to_string(),
-                        average_response_time_95_ms: row.get(1).unwrap().get_float().unwrap_or(0.0),
-                        count: row.get(2).unwrap().as_i64().unwrap_or(0) as u64,
-                        max_response_time_95_ms: row.get(3).unwrap().get_float().unwrap_or(0.0),
-                        min_response_time_95_ms: row.get(4).unwrap().get_float().unwrap_or(0.0),
-                    }
-                }).collect::<Vec<ExcelRow>>()
-            });
+        let res = wb.worksheet_range(sheet_name).map(|range| {
+            //skip the first row , for header
+            range
+                .rows()
+                .skip(1)
+                .map(|row| ExcelRow {
+                    service_name: row.get(0).unwrap().get_string().unwrap().to_string(),
+                    average_response_time_95_ms: row.get(1).unwrap().get_float().unwrap_or(0.0),
+                    count: row.get(2).unwrap().as_i64().unwrap_or(0) as u64,
+                    max_response_time_95_ms: row.get(3).unwrap().get_float().unwrap_or(0.0),
+                    min_response_time_95_ms: row.get(4).unwrap().get_float().unwrap_or(0.0),
+                })
+                .collect::<Vec<ExcelRow>>()
+        });
         res.unwrap()
     });
     let elapsed = now.elapsed();
@@ -53,7 +62,10 @@ fn load_excel(file_name: &String, sheet_name: &String) -> Result<Vec<ExcelRow>, 
     wb
 }
 
-fn import_database(connection: &Connection, excel_rows: &Vec<ExcelRow>) -> Result<(), sqlite::Error> {
+fn import_database(
+    connection: &mut Connection,
+    excel_rows: &Vec<ExcelRow>,
+) -> Result<(), rusqlite::Error> {
     use std::time::Instant;
     let now = Instant::now();
 
@@ -75,18 +87,17 @@ fn import_database(connection: &Connection, excel_rows: &Vec<ExcelRow>) -> Resul
         )
         ",
     )?;
-
-
     for row in excel_rows {
-        let service_name = &row.service_name;
-        stmt.bind((":service_name", service_name.as_str()))?;
-        stmt.bind((":average_response_time_95_ms", row.average_response_time_95_ms))?;
-        stmt.bind((":count", row.count as i64))?;
-        stmt.bind((":max_response_time_95_ms", row.max_response_time_95_ms))?;
-        stmt.bind((":min_response_time_95_ms", row.min_response_time_95_ms))?;
+        stmt.execute(named_params! {
+            ":service_name": &row.service_name,
+            ":average_response_time_95_ms": &row.average_response_time_95_ms,
+            ":count": &row.count,
+            ":max_response_time_95_ms": &row.max_response_time_95_ms,
+            ":min_response_time_95_ms": &row.min_response_time_95_ms
+        }).map(|ret|{
+            debug!("Insert excel row: {:?}", ret);
+        })?
     }
-
-
     let elapsed = now.elapsed();
     info!("Import data Elapsed: {:.2?}", elapsed);
 
@@ -95,11 +106,9 @@ fn import_database(connection: &Connection, excel_rows: &Vec<ExcelRow>) -> Resul
 
 fn main() {
     pretty_env_logger::init();
-
     let cli = Cli::parse();
 
-
-    let connection = sqlite::open(":memory:").unwrap();
+    let mut connection = Connection::open_in_memory().unwrap();
     let res = connection.execute(
         "
         CREATE TABLE excel_rows (
@@ -110,24 +119,43 @@ fn main() {
             min_response_time_95_ms REAL NOT NULL
         );
         ",
+        (),
     );
     match res {
         Ok(_) => {
             info!("Create table excel_rows successfully");
-            let excel_rows = load_excel(&cli.file_name,
-                                        &cli.sheet_name);
+            let excel_rows = load_excel(&cli.file_name, &cli.sheet_name);
             match excel_rows {
                 Ok(excel_rows) => {
                     info!("Load excel successfully");
-                    let res = import_database(&connection, &excel_rows);
+                    let res = import_database(&mut connection, &excel_rows);
                     match res {
                         Ok(_) => info!("Import excel rows successfully"),
-                        Err(e) => error!("Import excel rows error: {}",e)
+                        Err(e) => error!("Import excel rows error: {}", e),
                     }
                 }
-                Err(e) => error!("Load excel error: {}",e)
+                Err(e) => error!("Load excel error: {}", e),
             }
         }
-        Err(e) => error!("Create Table Error: {}",e)
+        Err(e) => error!("Create Table Error: {}", e),
+    }
+
+    let mut line = String::new();
+    loop {
+        println!("Please enter query statement > ");
+        io::stdin().read_line(&mut line).unwrap();
+        let mut stmt = connection.prepare(&line);
+        match stmt {
+            Ok(mut smt) => {
+                let mut rows = smt.query([]).unwrap();
+                while let Some(row) = rows.next().unwrap() {
+                    println!("row: {:?}", row);
+                }
+            }
+            Err(e) => {
+                error!("Statement error: {}", e);
+            }
+        }
+        line.clear();
     }
 }
