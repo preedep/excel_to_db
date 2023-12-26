3e7a6ffea2bb272c602ff163bc3ea005464b96ba
Preedee Ponchevin <nickmsft@mac.com>
2023-12-26 20:52:12 +0700

    Initial Project

--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,1 @@
-docker run -v /Users/preedee/Project/Rust/excel_to_db:/data/ -it excel2db:latest /app/excel_to_db -f /data/demo.xlsx -s Sheet1
+docker run --rm -v /Users/preedee/Project/Rust/excel_to_db:/data/ -it --entrypoint=/app/excel_to_db excel2db:latest -f /data/demo.xlsx -s Sheet1
